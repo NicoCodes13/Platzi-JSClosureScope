4ab68f32c50d12fbb101659bf10a20ded9db1246
Nico Castañeda <nickcar13@gmail.com>
2021-01-20 10:48:33 -0500

    global scope

---
+++ src/scope/global.js
@@ -0,0 +1,10 @@
+// Acceso global
+var hello = 'hello';
+let = world = 'Hello World';
+const helloWorld = 'Hello World!';
+
+const anotherFunction = () => {
+  console.log(hello);
+  console.log(world);
+  console.log(helloWorld);
+};
